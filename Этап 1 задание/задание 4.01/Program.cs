40fd02cfcb11a9a7f79750fdfecdc4fc7a68e025
southkek <maksim-nikitin-1993@mail.ru>
2022-01-05 19:25:19 +0600

    4.01

---
+++ Этап 1 задание/задание 4.01/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_4._01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double aMAX;
+            double bMAX;
+            if (a < b)
+            {
+                aMAX = b - a;
+                Console.WriteLine(aMAX);
+            }
+            else
+            {
+                bMAX = a - b;
+                Console.WriteLine(bMAX);
+            }
+
+        }
+    }
+}
